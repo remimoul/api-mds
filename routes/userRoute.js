45dba5c1df58d9593720c55c5f06b955317151b3
Rémi <115152594+remimoul@users.noreply.github.com>
2024-07-04 15:19:19 +0200

    Refactor userRoute.js to improve code readability and remove unnecessary routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,11 +4,8 @@ const userControllers = require('../controllers/userController');
 const jwtMiddleware = require('../middleware/jwtMiddleware');
 
 router.route('/login').post(userControllers.userLogin);
-
 router.route('/register').post(userControllers.createAUser);
-
 router.route('/update/:id').put(jwtMiddleware.verifyToken, userControllers.updateUser);
-
 router.route('/:user_id').put(userControllers.updateUser).delete(userControllers.deleteUser);
 
 module.exports = router;
